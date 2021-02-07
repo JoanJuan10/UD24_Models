0815aea61ebb7780b92ea564a2de0ec568b34e5b
JoanJuan10 <iijoanjuan10@gmail.com>
2021-02-07 10:47:13 +0100

    Ejercicio 1

---
+++ API_C06_ER_SQL/API_C06_ER_SQL/Models/Fabricante.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EX1.Models
+{
+    public partial class Fabricante
+    {
+        public Fabricante()
+        {
+            Articulos = new HashSet<Articulo>();
+        }
+
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public virtual ICollection<Articulo> Articulos { get; set; }
+
+    }
+}
